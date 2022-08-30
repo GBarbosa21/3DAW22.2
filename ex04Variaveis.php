f99649f60d9f982a049d0ed2453d8967503a5863
GBarbosa21 <99029205+GBarbosa21@users.noreply.github.com>
2022-08-30 09:30:25 -0300

    Create ex04Variaveis.php

---
+++ ex04Variaveis.php
@@ -0,0 +1,11 @@
+<?php
+    $nome1 = "jose";
+    $idade = 28;
+    $media = 7.5;
+
+    //concatenação se dá com .
+
+    echo "nome é tipo: " . var_dump($nome1);
+    echo "idade é tipo: " . var_dump($idade);
+    echo "media é tipo: " . var_dump($media);
+?>
