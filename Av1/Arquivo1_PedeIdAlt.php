b36cf6c2b6d4ae9b68ad93f77f79f50b1213d150
GBarbosa21 <99029205+GBarbosa21@users.noreply.github.com>
2022-10-04 21:58:57 -0300

    Add files via upload

---
+++ Av1/Arquivo1_PedeIdAlt.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <title>Pedir ID da Materia</title>
+</head>
+<body>
+    <form action="Arquivo1_Alterar2SGBD.php" method="POST">
+    ID da Materia <input type="text" name="mat"><br>
+    <input type="submit">
+    </form>
+</body>
+</html>
---
+++ Av1/Arquivo1_PedeMatAlt.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt">
+<head>
+    <title>Pedir Matricula</title>
+</head>
+<body>
+    <form action="Arquivo1_Alterar2SGBD.php" method="POST">
+    Matricula <input type="text" name="mat"><br>
+    <input type="submit">
+    </form>
+</body>
+</html>
