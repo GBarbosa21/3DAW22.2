2b17ee4b3fba9c281a445e7f856cd0fc956fb5ec
GBarbosa21 <99029205+GBarbosa21@users.noreply.github.com>
2022-10-11 11:54:05 -0300

    Add files via upload

---
+++ ex10IncluirAlunosJS.php
@@ -0,0 +1,27 @@
+<?php
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $bancodedados = "3dawmanha";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+        $nome = $_POST["nome"];
+        $mat = $_POST["mat"];
+        $email = $_POST["email"];
+
+        $conn = new mysqli($servidor, $usuario, $senha, $bancodedados);
+        if ($conn->connect_error) {
+            die("Erro de conexao com o banco de dados");
+        } 
+        //INSERT INTO `Alunos`(`Id`, `Nome`, `Matricula`, `Email`) VALUES ([value-1],[value-2],[value-3],[value-4])
+    //    $sql = "INSERT INTO `alunos`(`Id`, `Nome`, `Matricula`, `Email`) VALUES (1, '$nome', '$mat', '$email')";
+        $sql = "INSERT INTO `alunos`(`Nome`, `Matricula`, `Email`) VALUES ('$nome', '$mat', '$email')";
+        $resul = $conn->query($sql);
+        if ($resul == true){
+            echo "Confirmado!";
+        }
+        else{
+            echo "Deu Ruim!!";
+        }
+    }
+?>
